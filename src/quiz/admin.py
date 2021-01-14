caf6881140eff5b9ecf43fb33c3d94d32d9f1bda
Emre <emrekurtt1655@gmail.com>
2021-01-14 21:55:24 +0100

    add  extra and max

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -5,11 +5,15 @@ from .models import Category, Quiz, Question, Answer
 
 class AnswerInline(nested_admin.NestedTabularInline):  # ınline olarak hazırlıyor
     model = Answer
+    extra = 5
+    max_num = 8
 
 
 class QuestionInline(nested_admin.NestedTabularInline):
     model = Question
     inlines = [AnswerInline]
+    extra = 1
+    max_num = 10
 
 
 class QuizAdmin(nested_admin.NestedModelAdmin):
